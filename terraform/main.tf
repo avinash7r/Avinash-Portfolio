9382595cfe4afba97dd68fb771873531761c489d
Avinash <rajureavinash7@gmail.com>
2025-09-30 18:58:42 +0530

    feat: update S3 bucket name for Terraform backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-rajureavinash7"
     key            = "portfolio/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
