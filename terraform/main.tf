7bd6aa2f2bca8a33de906ea137839d399ac38316
Avinash <rajureavinash7@gmail.com>
2025-09-30 16:59:07 +0530

    feat: add Terraform backend configuration for S3 storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "portfolio/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+
+
 provider "aws" {
     region = "ap-south-1"
 }
